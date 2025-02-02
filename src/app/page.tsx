01a11a477f01dce9617d2f398ef0f69b92fdee58
Mohamed Belaouer <mohamedbelaouer93@gmail.com>
2025-02-02 12:19:46 +0100

    fix les problèmes avec sanity v6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import App from "@/components/App";
 
-export const dynamic = "force-static";
-export const revalidate = 5;
+export const dynamic = "force-dynamic";
+// export const revalidate = 5;
 export default function Home() {
   return (
     <>
