d877b6c65b8d142ee41bc81042822b0d271d81fc
Mohamed Belaouer <mohamedbelaouer93@gmail.com>
2026-06-24 23:38:08 +0200

    init project in new repo

--- src/app/api/google-reviews/route.ts
+++ src/app/api/google-reviews/route.ts
@@ -1,14 +1,16 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
 
-const GOOGLE_PLACES_API_KEY = process.env.GOOGLE_PLACES_API_KEY;
-const PLACE_ID = process.env.GOOGLE_PLACE_ID; // Stocker l'ID du restaurant dans .env.local
+const GOOGLE_PLACES_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY;
+const PLACE_ID = process.env.NEXT_PUBLIC_GOOGLE_PLACE_ID; // Stocker l'ID du restaurant dans .env.local
 
 export async function GET() {
   try {
     const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${PLACE_ID}&fields=reviews&key=${GOOGLE_PLACES_API_KEY}`;
 
     const response = await axios.get(url);
+    console.log(response.data);
+
     const data = response.data;
 
     if (data.status !== "OK") {
@@ -17,6 +19,9 @@ export async function GET() {
 
     return NextResponse.json(data.result.reviews);
   } catch (error) {
-    return NextResponse.json({ error: "Erreur lors de la récupération des avis" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Erreur lors de la récupération des avis" },
+      { status: 500 }
+    );
   }
 }
---
+++ src/components/Reviews.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Section from "./Section";
+
+type Review = {
+  author_name: string;
+  rating: number;
+  text: string;
+  profile_photo_url: string;
+};
+
+export default function GoogleReviews() {
+  const [reviews, setReviews] = useState<Review[]>([]);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const response = await axios.get("/api/google-reviews");
+        setReviews(response.data);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des avis:", error);
+      }
+    };
+
+    fetchReviews();
+  }, []);
+
+  return (
+    <Section id="reviews" title="avis">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 rounded-lg">
+        {reviews.length === 0 ? (
+          <p className="text-white text-center col-span-3">
+            Aucun avis disponible.
+          </p>
+        ) : (
+          reviews.map((avis, index) => (
+            <div key={index} className="bg-black/50 p-6 rounded-lg">
+              {/* Image de profil et nom du client */}
+              <div className="flex items-center space-x-3 mb-2">
+                <img
+                  src={avis.profile_photo_url}
+                  alt={avis.author_name}
+                  className="w-10 h-10 rounded-full border border-[#E4C590]"
+                />
+                <h2 className="text-xl font-semibold text-[#E4C590]">
+                  {avis.author_name}
+                </h2>
+              </div>
+
+              {/* Note sous forme d'étoiles */}
+              <div className="flex space-x-1 mb-4">
+                {[...Array(5)].map((_, index) => (
+                  <span
+                    key={index}
+                    className={`text-2xl ${
+                      index < avis.rating ? "text-[#E4C590]" : "text-gray-400"
+                    }`}
+                  >
+                    ★
+                  </span>
+                ))}
+              </div>
+
+              {/* Commentaire */}
+              <p className="text-white">{avis.text || "Aucun commentaire"}</p>
+            </div>
+          ))
+        )}
+      </div>
+    </Section>
+  );
+}
